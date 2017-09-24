a1a86db1386004b4e48f856eeb7956be0b8362b8
Michael van der Heide <vanderheide.michael@gmail.com>
2017-09-24 15:59:26 +0200

    Loads text into HTML with PageManager

---
+++ www/assets/include/loginPage.php
@@ -0,0 +1,6 @@
+<h1>Login</h1>
+<form action="./index.php" method="post">
+    Username: <input type="text"><br>
+    Passwort: <input type="password"><br>
+    <input type="submit" value="Login">
+</form>
