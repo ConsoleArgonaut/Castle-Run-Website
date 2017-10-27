9e54d3eda2b5de13cf88cbe2cac42585b1b62fae
Michael van der Heide <vanderheide.michael@gmail.com>
2017-10-27 13:33:09 +0200

    Fixed not loading side bar bug

--- www/assets/include/pageManager.php
+++ www/assets/include/pageManager.php
@@ -39,19 +39,17 @@ if (isset($_POST['register'])){
             //echo '<h1>Title</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>';
 
         } else {
-            $error = "Username or Password is invalid";
-            echo $error;
+            if(isset($_SESSION['login_failure']) && $_SESSION['login_failure'] == 'true'){
+                $error = "Username or Password is invalid";
+                echo $error;
+            }
             echo file_get_contents('./assets/include/loginPage.php');
 
         }
     }
 
 function isLoggedIn() {
-    $loggedin = false;
-
-    if (isset($_SESSION['login_user'])) {
-        $loggedin = true;
-    }
+    if (isset($_SESSION['login_user'])){}
     else if(isset($_POST['submit'])) {
             if (isset($_POST['username']) && isset($_POST['password'])) {
                 $verify = $_POST["password"];
@@ -70,6 +68,13 @@ function isLoggedIn() {
                 }
             }
         }
+
+    $loggedin = false;
+
+    if (isset($_SESSION['login_user'])) {
+        $loggedin = true;
+
+    }
     return $loggedin;
 }
 
